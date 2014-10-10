d15dcb466b81010176929210c1a5a413291f5144
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-10-09 21:58:43 -0300

    Created .create_notification and #render

--- lib/generators/act_as_notificable/templates/notification.rb
+++ lib/generators/act_as_notificable/templates/notification.rb
@@ -4,4 +4,21 @@ class Notification < ActiveRecord::Base
   validates :template_name, presence: true
 
   serialize :locals, Hash
+
+  def render
+    locals.merge!(notification: self)
+    view.render(file: "notifications/#{template_name}", locals: locals)
+  end
+
+  def self.create_notification(template_name, locals = {})
+    Notification.create(template_name: template_name, locals: locals)
+  end
+
+  private
+    def view
+      view_paths = ActionController::Base.view_paths
+      @_view ||= Class.new(ActionView::Base) do
+        include Rails.application.routes.url_helpers
+      end.new(view_paths, {})
+    end
 end
--- spec/notification_spec.rb
+++ spec/notification_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe Notification, type: :model do
   describe "associations" do
     it { should belong_to(:owner) }
   end
+
+  describe ".create_notification" do
+    subject do
+      Notification.create_notification(:welcome)
+    end
+
+    it { expect(subject.template_name).to eq(:welcome)}
+    it { expect(subject).to be_persisted }
+  end
+
+  describe "#render" do
+    subject do
+      Notification.create_notification(:welcome)
+    end
+
+    it { expect(subject.render).to include("Welcome to my world") }
+  end
+
 end
