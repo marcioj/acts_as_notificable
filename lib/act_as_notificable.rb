c496cadfc90265a2d4a6de4abeceb5259d584ec4
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-10-11 17:08:31 -0300

    Tests for act_as_notificable concern

--- lib/act_as_notificable.rb
+++ lib/act_as_notificable.rb
@@ -1,2 +1,11 @@
 module ActAsNotificable
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def act_as_notificable
+      has_many :notifications, inverse_of: "owner"
+    end
+  end
 end
+
+ActiveRecord::Base.send :include, ActAsNotificable
--- spec/support/active_record.rb
+++ spec/support/active_record.rb
@@ -1,4 +1,7 @@
 require "generators/act_as_notificable/templates/migration"
 
+ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
 ActiveRecord::Migrator.new(:up, [CreateNotificationsTable.new]).migrate
+
+load(File.expand_path(File.join('..', '..', 'dummy', 'db', 'schema.rb'), __FILE__))
